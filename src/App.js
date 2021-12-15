9c75d8fd09af0054f103f8589838e7dd2380f952
jcpcabanada <jcpcabanada@gmail.com>
2021-12-14 22:51:56 -0800

    made new repo maybe messed up npm i first time

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+
+import Login from "./components/Login";
+import FriendList from "./components/FriendsList";
+import AddFriends from "./components/AddFriend";
 
 function App() {
-  return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h2>FRIENDS DATABASE</h2>
+            <Route exact path="/">
+                <Login/>
+            </Route>
+            <Route exact path="/login">
+                <Redirect to="/"/>
+            </Route>
+            <Route exact path="/friends">
+                <FriendList/>
+            </Route>
+            <Route exact path="/friends/add">
+                <AddFriends/>
+            </Route>
+        </div>
+
+    );
 }
 
 export default App;
