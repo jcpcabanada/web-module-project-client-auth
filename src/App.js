183136b99cbba199556441b9e85da58a163a361d
jcpcabanada <jcpcabanada@gmail.com>
2021-12-15 00:18:00 -0800

    my friendsList not working something with the axios get keep on getting 403

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Redirect, Link} from 'react-router-dom';
 
 import Login from "./components/Login";
 import FriendList from "./components/FriendsList";
@@ -8,21 +8,29 @@ import AddFriends from "./components/AddFriend";
 
 function App() {
     return (
-        <div className="App">
-            <h2>FRIENDS DATABASE</h2>
-            <Route exact path="/">
-                <Login/>
-            </Route>
-            <Route exact path="/login">
-                <Redirect to="/"/>
-            </Route>
-            <Route exact path="/friends">
-                <FriendList/>
-            </Route>
-            <Route exact path="/friends/add">
-                <AddFriends/>
-            </Route>
-        </div>
+        <Router>
+            <div className="App">
+                <header>
+                    <h2>FRIENDS DATABASE</h2>
+                    <Link className="links" to="/login" >LOGIN.</Link>
+                    <Link className="links" to="/friends" >FRIENDSLIST.</Link>
+                    <Link className="links" to="/friends/add" >ADDFRIEND.</Link>
+                    <Link className="links" to="/friends" >LOGOUT.</Link>
+                </header>
+                <Route exact path="/">
+                    <Login/>
+                </Route>
+                <Route exact path="/login">
+                    <Redirect to="/"/>
+                </Route>
+                <Route exact path="/friends">
+                    <FriendList/>
+                </Route>
+                <Route exact path="/friends/add">
+                    <AddFriends/>
+                </Route>
+            </div>
+        </Router>
 
     );
 }
